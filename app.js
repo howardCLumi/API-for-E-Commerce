aff52c608b9e0ee89934772b5491e3a1941e766c
howardCLumi <102765211+howardCLumi@users.noreply.github.com>
2023-10-26 00:15:45 -0500

    add functionality to CRUD operations.

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/products");
+const ordersRoutes = require("./api/routes/orders");
+
+mongoose.connect(
+  "mongodb+srv://node-shop:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0.p3gbtlj.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.use(morgan("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+app.use((res, req, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+// These two are middlewares
+app.use("/products", productRoutes);
+app.use("/orders", ordersRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
